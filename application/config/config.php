d9f697cc9a1f31521502fd26730d19f988503498
septyanctr <septyanctr@gmail.com>
2024-05-14 16:23:47 +0700

    Tugas 2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] ='http://localhost/pustaka-booking/assets/css/stylebuku.css';
+$config['base_url'] ='http://localhost/pustaka-booking/';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -2,14 +2,17 @@
 defined('BASEPATH') or exit ('no direct script acces allowed');
 
 class Web extends CI_Controller{
-    function__construct(){
+    function __construct()
+    {
         parent::__construct();
+        
     }
 
-    public function index(){
-        $data['judul']= "Halaman Depan";
-        $this->load->view('v_header'. $data);
-        $this->load->view('v_index'. $data);
-        $this->load->view('v_footer'. $data);
+    public function index()
+    {
+        $data["judul"]= "Halaman Depan";
+        $this->load->view("v_header", $data);
+        $this->load->view("v_index", $data);
+        $this->load->view("v_footer", $data);
     }
 }
